84e69b26d304a5dc4e3bac23ef222a49ee106ca7
SHA3L <mr.sahil.in@gmail.com>
2025-05-13 20:49:43 +1200

    Create home_screen.dart
    Add home_screen.dart with GPS location feature

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String locationMessage = 'Tap the button to get GPS location';
+
+  Future<void> getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        locationMessage = 'Location services are disabled.';
+      });
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          locationMessage = 'Location permissions are denied';
+        });
+        return;
+      }
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      locationMessage =
+          'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MowNgo Dashboard')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(locationMessage, textAlign: TextAlign.center),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: getCurrentLocation,
+              child: const Text('Get GPS Location'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
